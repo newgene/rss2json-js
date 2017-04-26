432c301f51f5b358a79ecc061db4e630996af6c0
Chunlei Wu <anewgene@yahoo.com>
2017-04-26 12:24:28 -0700

    updated default port number.

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ A NodeJS API parsing a RSS feed into a JSON object
 - `npm install` to install the dependencies
 - `npm start` to run it
 
-- run it at "http://localhost:8002/rss2json?feed=<feed_url>"
+  - test it at "http://localhost:8854/rss2json?feed=<feed_url>"
--- rss2json.js
+++ rss2json.js
@@ -4,7 +4,7 @@ var express = require('express');
 var request = require('request');
 var app = express();
 var router = express.Router();
-var port = process.env.PORT || 8002;
+var port = process.env.PORT || 8854;
 
 var parser = new require('xml2js').Parser({
     "explicitArray": false
